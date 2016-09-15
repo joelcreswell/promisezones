fa33a145013d7d28c278cf8fdc7e306a8234eceb
Creswell <Creswell.Joel@epa.gov>
2016-09-15 16:36:17 -0400

    Updating sandbox file for use by others (not Joel)

--- sandbox.R
+++ sandbox.R
@@ -1,4 +1,14 @@
+##Subset variables from ejscreen for sharing as ejair
+
 ejair <- data.frame(ejscreen$DSLPM, ejscreen$PM25,
                     ejscreen$D_DSLPM_2, ejscreen$D_PM25_2)
 
 names(ejair) <- c("Diesel PM", "PM2.5", "EJDiesel", "EJPM2.5")
+
+##Save ejair as CSV file
+
+write.csv(ejair, file = "ejair.csv")
+
+##Read in ejair
+
+ejair <- read.csv("ejair.csv")
