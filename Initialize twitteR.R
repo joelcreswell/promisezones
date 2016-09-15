92ed0ad660032d1b0b489d577e143d6c03cd3ab1
Creswell <Creswell.Joel@epa.gov>
2016-09-15 17:11:36 -0400

    Added instructions for how to initialize twitter session

--- Initialize twitteR.R
+++ Initialize twitteR.R
@@ -1,8 +1,15 @@
+library(twitteR)
+
+##Go to this web page to create your Twitter developer account:
+## https://www.r-bloggers.com/setting-up-the-twitter-r-package-for-text-analytics/
+
 ##Set key, token, secrets
 
-consumer_key <- "gskubcc1Zee2CtF8cq5nL0sOt"
-consumer_secret <- "asvO3Y7OZcOyDcniYhnM3AU0G5HoFqMRmnx2FoIAVwYiz6Ftv0"
-access_token <- "19549553-RqrRhnMGetWhSSTLQXyr08N0MGd2wExC30c52LGXg"
-access_secret <- "IxlVFgFRmRikHCvZuL461ndIiRlj9o5REXz1WnMFFXclS"
+consumer_key <- "_your key here_"
+consumer_secret <- "_your secret here_"
+access_token <- "_your token here_"
+access_secret <- "_your secret here_"
+
+##Authorize your Twitter session
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
