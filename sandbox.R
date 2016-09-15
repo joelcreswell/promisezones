631fa3e0d1f56091ef5033d06833789bb10466d0
Creswell <Creswell.Joel@epa.gov>
2016-09-15 16:49:48 -0400

    Updated ejair var to include state and FIPS code.

--- sandbox.R
+++ sandbox.R
@@ -1,9 +1,9 @@
 ##Subset variables from ejscreen for sharing as ejair
 
-ejair <- data.frame(ejscreen$DSLPM, ejscreen$PM25,
-                    ejscreen$D_DSLPM_2, ejscreen$D_PM25_2)
+ejair <- data.frame(ejscreen$ID, ejscreen$ST_ABBREV,
+                    ejscreen$PM25, ejscreen$D_PM25_2)
 
-names(ejair) <- c("Diesel PM", "PM2.5", "EJDiesel", "EJPM2.5")
+names(ejair) <- c("FIPS", "State", "PM2.5", "EJPM2.5")
 
 ##Save ejair as CSV file
 
@@ -12,3 +12,7 @@ write.csv(ejair, file = "ejair.csv")
 ##Read in ejair
 
 ejair <- read.csv("ejair.csv")
+
+##Get rid of empty columns in ejair
+
+ejair <- ejair[,-c(2,4)]
